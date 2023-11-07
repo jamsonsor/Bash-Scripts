a9bb46badc4e8d42643efd2fafe2902888ab7b36
jamsonsor <jamsons93@gmail.com>
2023-11-07 00:33:52 -0500

    updated the script with more api calls

--- cloudflare-practice/api-practice.sh
+++ cloudflare-practice/api-practice.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -x
+set +x
 
 source ../.env
 
 curl --request GET \
   --url https://api.cloudflare.com/client/v4/user \
   --header 'Content-Type: application/json' \
-  --header "X-Auth-Email: $CF_TOKEN"
+  --header "X-Auth-Email: $CF_EMAIL" \
+  --header "X-Auth-Key: $CF_GKEY" | jq .result.username
+
+curl --request GET \
+  --url https://api.cloudflare.com/client/v4/zones \
+  --header "Authorization: Bearer $CF_READ_TOKEN" \
+  --header 'Content-Type: application/json' | jq .result[].name
