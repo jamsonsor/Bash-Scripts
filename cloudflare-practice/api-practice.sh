36e46b040eaa2601bb3f840e2cf3726d36dfdb60
jamsonsor <jamsons93@gmail.com>
2023-11-07 00:18:37 -0500

    added get-user request

--- cloudflare-practice/api-practice.sh
+++ cloudflare-practice/api-practice.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+set -x
+
+source ../.env
+
+curl --request GET \
+  --url https://api.cloudflare.com/client/v4/user \
+  --header 'Content-Type: application/json' \
+  --header "X-Auth-Email: $CF_TOKEN"
