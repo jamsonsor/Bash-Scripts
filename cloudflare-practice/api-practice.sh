892e77d3492890f06ee406b4067bf792e4c3ae82
jamsonsor <jamsons93@gmail.com>
2023-11-08 23:58:08 -0500

    added another api request

--- cloudflare-practice/api-practice.sh
+++ cloudflare-practice/api-practice.sh
@@ -14,3 +14,9 @@ curl --request GET \
   --url https://api.cloudflare.com/client/v4/zones \
   --header "Authorization: Bearer $CF_READ_TOKEN" \
   --header 'Content-Type: application/json' | jq .result[].name
+
+curl --request GET \
+  --url https://api.cloudflare.com/client/v4/zones/$CF_ZONEID/firewall/rules \
+  --header 'Content-Type: application/json' \
+  --header "X-Auth-Email: $CF_EMAIL" \
+  --header "X-Auth-Key: $CF_GKEY" | jq .
