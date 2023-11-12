6def2dc8016c79acb955745b18148de384e08ce0
jamsonsor <jamsons93@gmail.com>
2023-11-11 21:28:32 -0500

    added page rule create and delete requests

--- cloudflare-practice/api-practice.sh
+++ cloudflare-practice/api-practice.sh
@@ -20,3 +20,34 @@ curl --request GET \
   --header 'Content-Type: application/json' \
   --header "X-Auth-Email: $CF_EMAIL" \
   --header "X-Auth-Key: $CF_GKEY" | jq .
+
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/$CF_ZONEID/pagerules \
+  --header 'Content-Type: application/json' \
+  --header "X-Auth-Email: $CF_EMAIL" \
+  --header "X-Auth-Key: $CF_GKEY" \
+  --data '{
+  "actions": [
+    {
+      "id": "browser_check",
+      "value": "on"
+    }
+  ],
+  "priority": 1,
+  "status": "active",
+  "targets": [
+    {
+      "constraint": {
+        "operator": "matches",
+        "value": "*devopsjam.com/images/*"
+      },
+      "target": "url"
+    }
+  ]
+}' | jq .
+
+curl --request DELETE \
+  --url https://api.cloudflare.com/client/v4/zones/$CF_ZONEID/pagerules/f55cab1c756e9efe5af4e5d0356d19dc \
+  --header 'Content-Type: application/json' \
+  --header "X-Auth-Email: $CF_EMAIL" \
+  --header "X-Auth-Key: $CF_GKEY" | jq .
