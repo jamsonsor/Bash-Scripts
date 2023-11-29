ca86669786128b60551b3cfc18229b0b408702f3
jamsonsor <jamsons93@gmail.com>
2023-11-28 23:52:10 -0500

    added page rule for home endpoint

--- cloudflare-practice/api-practice.sh
+++ cloudflare-practice/api-practice.sh
@@ -46,6 +46,31 @@ curl --request POST \
   ]
 }' | jq .
 
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/$CF_ZONEID/pagerules \
+  --header 'Content-Type: application/json' \
+  --header "X-Auth-Email: $CF_EMAIL" \
+  --header "X-Auth-Key: $CF_GKEY" \
+  --data '{
+  "actions": [
+    {
+      "id": "browser_check",
+      "value": "on"
+    }
+  ],
+  "priority": 1,
+  "status": "active",
+  "targets": [
+    {
+      "constraint": {
+        "operator": "matches",
+        "value": "*devopsjam.com/home"
+      },
+      "target": "url"
+    }
+  ]
+}' | jq .
+
 curl --request DELETE \
   --url https://api.cloudflare.com/client/v4/zones/$CF_ZONEID/pagerules/f55cab1c756e9efe5af4e5d0356d19dc \
   --header 'Content-Type: application/json' \
